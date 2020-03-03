9b7f4e64754d8fee4c6463cb97f9a7c2ede5e3de
Will <williamgarrofeb4@gmail.com>
2020-03-03 12:59:15 +0100

    Modif Indicateur

--- app/src/main/java/com/example/journal_perso/ConfigurationIndicateur.java
+++ app/src/main/java/com/example/journal_perso/ConfigurationIndicateur.java
@@ -6,26 +6,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
-import android.widget.Toast;
 
-public class ConfigurationIndicateur extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ConfigurationIndicateur extends AppCompatActivity  implements AdapterView.OnItemSelectedListener{
 
-    private CheckBox maCheckBox = findViewById(R.id.checkBoxIndicateur);
-    private EditText monEditTextIndicateur =  findViewById(R.id.editTextIndicateur);
-    private EditText monIndicateurNom =  findViewById(R.id.nomIndicateur);
-    private Switch monSwitchCreneau = findViewById(R.id.switchCreneau);
-    private EditText monTempsCreneau = findViewById(R.id.tempsCreneau);
+    private CheckBox maCheckBox;
+    private EditText monEditTextIndicateur;
+    private EditText monIndicateurNom;
+    private Switch monSwitchCreneau;
+    private EditText monTempsCreneau;
+    private Button monButon;
+    private Spinner monSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration_indicateur);
 
-        Spinner monSpinner = (Spinner) findViewById(R.id.typeIndicateur);
+        maCheckBox = findViewById(R.id.checkBoxIndicateur);
+        monEditTextIndicateur =  findViewById(R.id.editTextIndicateur);
+        monIndicateurNom =  findViewById(R.id.nomIndicateur);
+        monSwitchCreneau = findViewById(R.id.switchCreneau);
+        monTempsCreneau = findViewById(R.id.tempsCreneau);
+        monButon = findViewById(R.id.buttonAjoutIndicateur);
+        monSpinner = findViewById(R.id.typeIndicateur);
+
         ArrayAdapter<CharSequence> monAdaptater = ArrayAdapter.createFromResource(this, R.array.typeIndicateurNom,android.R.layout.simple_spinner_item);
         monAdaptater.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         monSpinner.setAdapter(monAdaptater);
@@ -39,11 +48,13 @@ public class ConfigurationIndicateur extends AppCompatActivity implements Adapte
             maCheckBox.setVisibility(View.VISIBLE);
             monSwitchCreneau.setVisibility(View.VISIBLE);
             monTempsCreneau.setVisibility(View.VISIBLE);
+            monButon.setVisibility(View.VISIBLE);
         }
         else{
             monEditTextIndicateur.setVisibility(View.VISIBLE);
             monSwitchCreneau.setVisibility(View.VISIBLE);
             monTempsCreneau.setVisibility(View.VISIBLE);
+            monButon.setVisibility(View.VISIBLE);
         }
     }
 
