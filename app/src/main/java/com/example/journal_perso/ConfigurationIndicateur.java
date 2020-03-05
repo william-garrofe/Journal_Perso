0c75d7575b98f28c9b2bf0e55f796d8638c5c989
Will <williamgarrofeb4@gmail.com>
2020-03-05 13:04:23 +0100

    Debut sauvegarde Donnees

--- app/src/main/java/com/example/journal_perso/ConfigurationIndicateur.java
+++ app/src/main/java/com/example/journal_perso/ConfigurationIndicateur.java
@@ -1,7 +1,11 @@
 package com.example.journal_perso;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.icu.text.CaseMap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,6 +16,9 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 
+import java.util.Map;
+import java.util.Set;
+
 public class ConfigurationIndicateur extends AppCompatActivity  implements AdapterView.OnItemSelectedListener{
 
     private CheckBox maCheckBox;
@@ -19,8 +26,10 @@ public class ConfigurationIndicateur extends AppCompatActivity  implements Adapt
     private EditText monIndicateurNom;
     private Switch monSwitchCreneau;
     private EditText monTempsCreneau;
-    private Button monButon;
+    private Button monButton;
     private Spinner monSpinner;
+    private SharedPreferences mesPreferences;
+    private int numIndic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,29 +41,46 @@ public class ConfigurationIndicateur extends AppCompatActivity  implements Adapt
         monIndicateurNom =  findViewById(R.id.nomIndicateur);
         monSwitchCreneau = findViewById(R.id.switchCreneau);
         monTempsCreneau = findViewById(R.id.tempsCreneau);
-        monButon = findViewById(R.id.buttonAjoutIndicateur);
+        monButton = findViewById(R.id.buttonAjoutIndicateur);
         monSpinner = findViewById(R.id.typeIndicateur);
 
         ArrayAdapter<CharSequence> monAdaptater = ArrayAdapter.createFromResource(this, R.array.typeIndicateurNom,android.R.layout.simple_spinner_item);
         monAdaptater.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         monSpinner.setAdapter(monAdaptater);
         monSpinner.setOnItemSelectedListener(this);
+
+        SharedPreferences mesPreferences = getSharedPreferences("mesPreferences", Context.MODE_PRIVATE);
+
+        monButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Save(v,numIndic);
+                Save(v);
+            }
+        });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String monText = parent.getItemAtPosition(position).toString();
-        if(position == 1) {
-            maCheckBox.setVisibility(View.VISIBLE);
-            monSwitchCreneau.setVisibility(View.VISIBLE);
-            monTempsCreneau.setVisibility(View.VISIBLE);
-            monButon.setVisibility(View.VISIBLE);
-        }
-        else{
-            monEditTextIndicateur.setVisibility(View.VISIBLE);
-            monSwitchCreneau.setVisibility(View.VISIBLE);
-            monTempsCreneau.setVisibility(View.VISIBLE);
-            monButon.setVisibility(View.VISIBLE);
+        numIndic = position;
+        switch (position)
+        {
+            case 1:
+                maCheckBox.setVisibility(View.VISIBLE);
+                monSwitchCreneau.setVisibility(View.VISIBLE);
+                monTempsCreneau.setVisibility(View.VISIBLE);
+                monButton.setVisibility(View.VISIBLE);
+                break;
+
+            case 2:
+                monIndicateurNom.setVisibility(View.INVISIBLE);
+                monEditTextIndicateur.setVisibility(View.VISIBLE);
+                monSwitchCreneau.setVisibility(View.VISIBLE);
+                monTempsCreneau.setVisibility(View.VISIBLE);
+                monButton.setVisibility(View.VISIBLE);
+                break;
+
         }
     }
 
@@ -66,5 +92,21 @@ public class ConfigurationIndicateur extends AppCompatActivity  implements Adapt
         monEditTextIndicateur.setVisibility(View.INVISIBLE);
     }
 
+    public void Save(View view) {
+        int typeIndic=0;
+        String n;
+        if (typeIndic == 1)
+        {
+            n = monIndicateurNom.getText().toString();
+        }else{
+            n = monEditTextIndicateur.getText().toString();
+        }
+
+        //String e = email.getText().toString();
+        SharedPreferences.Editor editor = mesPreferences.edit();
+        editor.putString("NameAppli", n);
+        //editor.putString(Email, e);
+        editor.commit();
+    }
 
 }
