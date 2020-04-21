d3d59bcd92ff0b6ea524550ccc165c8fbd83baf8
Will <williamgarrofeb4@gmail.com>
2020-04-21 09:07:34 +0200

    add espace to calendrier

--- app/src/main/java/com/example/journal_perso/ui/calendrier/CalendrierFragment.java
+++ app/src/main/java/com/example/journal_perso/ui/calendrier/CalendrierFragment.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,6 +24,7 @@ import java.util.Vector;
 public class CalendrierFragment extends Fragment {
 
     private CalendrierViewModel calendrierViewModel;
+    private espace mesEsp;
     final private gsonFic gf = new gsonFic();
     private ListView listCal;
     private String nomEspace;
@@ -35,31 +36,46 @@ public class CalendrierFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_calendrier, container, false);
 
         final CalendarView cal = root.findViewById(R.id.calendarView);
-        listCal = root.findViewById(R.id.listCalendrier);
+        //listCal = root.findViewById(R.id.listCalendrier);
         final ArrayList<String> list = new ArrayList<>();
+        final LinearLayout ll = root.findViewById(R.id.maLayoutCalendrier);
 
         maData maDatas = gf.LireFichier(getContext(), "monJson.json");
+        final maData finalData = maDatas;
 
         if (maDatas == null) {
             maDatas = new maData();
             maDatas.setMesEspaces(new Vector<espace>());
         }
 
+
         for (int i = 0; i < maDatas.getMesEspaces().size(); i++) {
             nomEspace = maDatas.getMesEspaces().get(i).nomEsp();
+
             list.add(nomEspace);
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list); //items maDatas.getMesEspaces()
-        listCal.setAdapter(adapter);
+
+       /* ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list); //items maDatas.getMesEspaces()
+        listCal.setAdapter(adapter);*/
 
         long test = cal.getDate();
 
         cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                Toast patate = Toast.makeText(getContext(), "test", Toast.LENGTH_LONG);
-                patate.show();
+                ll.removeAllViews();
+                for (int i = 0; i < finalData.getMesEspaces().size(); i++) {
+                    if (finalData.getMesEspaces().get(i).getListJour().contains(dayOfMonth)) {
+                        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                        Button nB = new Button(getContext());
+                        nB.setLayoutParams(p);
+                        nB.setHint("Texte");
+                        ll.addView(nB);
+                      /* Toast patate = Toast.makeText(getContext(), finalData.getMesEspaces().get(i).getNom(), Toast.LENGTH_LONG);
+                       patate.show();*/
+                    }
+                }
             }
         });
 
